cc57e95429043c7ac4a99c009f9baa49742f8b68
MustafaBalila44 <hitsugayatoshiro899@gmail.com>
2019-01-30 07:04:17 +0200

    Added more HTML to the lists home

--- functional_tests.py
+++ functional_tests.py
@@ -22,16 +22,17 @@ class NewVisitorTest(unittest.TestCase):
 
         # testing user input "the to do input feild"
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual('Enter a to do item',
+        self.assertEqual('Enter a to-do item',
                          inputbox.get_attribute('placeholder'))
         # simulates typing in the input feild
         inputbox.send_keys('Buy some milk!')
-        # simulates hitting enter 
+        # simulates hitting enter
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy some milk!' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy some milk!' for row in rows),
+                        "New to-do item did not appear in table")
         self.fail('End of the test')
 
 
